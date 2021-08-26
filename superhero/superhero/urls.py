e548cfdb73c26f44e297a7cb1fb0246106453f60
Djaypm89 <djaypm89@gmail.com>
2021-08-26 17:54:06 -0400

    vid 7 push, except or detail.html file

--- superhero/superhero/urls.py
+++ superhero/superhero/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('superheroes/', include('superheroes.url')),
+    path('superheroes/', include('superheroes.urls')),
     path('', RedirectView.as_view(url='/superheroes/')),
 ]
